6aee36fbf26db78d0c1cd65c751469dd062366a4
Alex-Reyne <creativereyne@gmail.com>
2021-10-07 10:22:43 -0600

    Made sure file was saved

--- conditionals.js
+++ conditionals.js
@@ -2,9 +2,10 @@ const raining = false;
 const temperature = 20;
 
 if (raining) {
-  console.log("Don't forget your umbrella!");
+  console.log("Don't forget your umbrella!"); // prints if it's raining outside.
 }
 
+//suggests when you should bring a scarf, t-shirt, or other outerwear.
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
@@ -17,15 +18,16 @@ console.log("Now you're ready to go outside!");
 
 const isCitizen = true;
 const age = 26;
-
+//prints if you are old enough to vote.
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
-
+//suggests not going outside if the weather is extreme.
 if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a great idea...");
 }
-
+//reminds you not to bring your umbrella if it's nice out.
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+
