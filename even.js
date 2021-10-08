93e767e2fbdbe7924082f7b18cae45db12651f16
Alex-Reyne <creativereyne@gmail.com>
2021-10-08 14:43:06 -0600

    idk , I must have added a space to even.js

--- even.js
+++ even.js
@@ -1,12 +1,16 @@
+// returns true or false if (num) is even or odd respectively.
 const isEven = function(num) {
   return num % 2 === 0;
 }
 
+/*
 const tenIsEven = isEven(10);
 const elevenIsEven = isEven(11);
 
 console.log(tenIsEven);
 console.log(elevenIsEven);
+*/
 
 console.log(isEven(10));
 console.log(isEven(11));
+
